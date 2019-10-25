826ed9bcc5e73b1521cca79a4d97f65c619d30dd
Nancy Savoie <nancysavoiegiles@gmail.com>
2019-10-24 22:55:13 -0400

    Ex6: Prey script - Error 2

--- Exercises/exercise6/js/Prey.js
+++ Exercises/exercise6/js/Prey.js
@@ -14,7 +14,7 @@ class Prey { //FIXED: Class was spelled "glass"
   //
   // Sets the initial values for the Predator's properties
   // Either sets default values or uses the arguments provided
-  constructor(x, why, speed, fillColor, radius) {
+  constructor(x, y, speed, fillColor, radius) { //FIXED: Why? It's y!
     // Position
     this.x = x;
     this.y = y;
