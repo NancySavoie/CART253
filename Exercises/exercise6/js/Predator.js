90ddbf9ec18b4c5ec4abeb0adca2f537ae48f0aa
Nancy Savoie <nancysavoiegiles@gmail.com>
2019-10-24 22:36:28 -0400

    Ex6: Predator script - Error 3

--- Exercises/exercise6/js/Predator.js
+++ Exercises/exercise6/js/Predator.js
@@ -29,7 +29,7 @@ class Predator {
     this.healthGainPerEat = 1;
     // Display properties
     this.fillColor = fillColor;
-    this.radios = this.health; // Radius is defined in terms of health
+    this.radius = this.health; // FIXED: Radius was spelled "radios"
     // Input properties
     this.appKey = UP_ARROW;
     this.downKey = DOWN_ARROW;
